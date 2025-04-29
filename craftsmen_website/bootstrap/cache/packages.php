f228a2d3aaed567f75ded58cbf3a91dcb8ebe169
OmarBayazeed <102438534+OmarBayazeed@users.noreply.github.com>
2025-04-29 22:33:48 +0300

    Add files via upload

---
+++ craftsmen_website/bootstrap/cache/packages.php
@@ -0,0 +1,89 @@
+<?php return array (
+  'bepsvpt/blurhash' => 
+  array (
+    'aliases' => 
+    array (
+      'BlurHash' => 'Bepsvpt\\Blurhash\\Facades\\BlurHash',
+    ),
+    'providers' => 
+    array (
+      0 => 'Bepsvpt\\Blurhash\\BlurHashServiceProvider',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'intervention/image-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\Laravel\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Laravel\\Facades\\Image',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'tymon/jwt-auth' => 
+  array (
+    'aliases' => 
+    array (
+      'JWTAuth' => 'Tymon\\JWTAuth\\Facades\\JWTAuth',
+      'JWTFactory' => 'Tymon\\JWTAuth\\Facades\\JWTFactory',
+    ),
+    'providers' => 
+    array (
+      0 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
+    ),
+  ),
+);
---
+++ craftsmen_website/config/firebase.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'credentials' => storage_path('app/craftsman-7f136-firebase-adminsdk-fbsvc-d43dde50c5.json'),
+    'database_url' => env('FIREBASE_DATABASE_URL'),
+];
+
+?>
